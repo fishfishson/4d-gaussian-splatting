db7097cdc039e952505a98bffa690e17d78b1149
yuzhiyuan <zyuaq@ust.hk>
2024-05-12 16:24:57 +0800

    fix render env_map_res

--- render.py
+++ render.py
@@ -8,12 +8,14 @@
 #
 # For inquiries contact  george.drettakis@inria.fr
 #
-
+import sys
 import torch
 from scene import Scene
 import os
 from tqdm import tqdm
 from os import makedirs
+from omegaconf import OmegaConf
+from omegaconf.dictconfig import DictConfig
 from gaussian_renderer import render
 import torchvision
 from utils.general_utils import safe_state
@@ -42,23 +44,27 @@ def render_sets(dataset : ModelParams, iteration : int, pipeline : PipelineParam
         bg_color = [1,1,1] if dataset.white_background else [0, 0, 0]
         background = torch.tensor(bg_color, dtype=torch.float32, device="cuda")
 
-        if not skip_train:
-             render_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background)
-
-        if not skip_test:
-             render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background)
+        # if not skip_train:
+        #     resnder_set(dataset.model_path, "train", scene.loaded_iter, scene.getTrainCameras(), gaussians, pipeline, background)
+        
+        # if not skip_test:
+        render_set(dataset.model_path, "test", scene.loaded_iter, scene.getTestCameras(), gaussians, pipeline, background)
 
 if __name__ == "__main__":
     # Set up command line argument parser
     parser = ArgumentParser(description="Testing script parameters")
     model = ModelParams(parser, sentinel=True)
     pipeline = PipelineParams(parser)
+    parser.add_argument("--config", type=str)
     parser.add_argument("--iteration", default=-1, type=int)
     parser.add_argument("--skip_train", action="store_true")
     parser.add_argument("--skip_test", action="store_true")
     parser.add_argument("--quiet", action="store_true")
     args = get_combined_args(parser)
     print("Rendering " + args.model_path)
+
+    cfg = OmegaConf.load(args.config)
+    args.env_map_res = cfg.PipelineParams.env_map_res
 
     # Initialize system state (RNG)
     safe_state(args.quiet)
